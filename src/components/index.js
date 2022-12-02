98a489149d7a87d6c40c67eafc347f93e5e4f9fe
Steven Seley Jr <seleysteven2@gmail.com>
2022-12-01 23:26:42 -0600

    base sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { default as Feed } from './Feed';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
 
