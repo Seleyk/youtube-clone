654bf2d7bb8f5c91aa6ecfff40e5467df723d38d
Steven Seley Jr <seleysteven2@gmail.com>
2022-12-04 23:19:39 -0600

    bug: wont render videos or log it

---
+++ src/components/Videos.jsx
@@ -0,0 +1,28 @@
+import { ChannelCard, VideoCard } from './'
+
+import { Stack, Box } from '@mui/material'
+
+const Videos = ({ videos }) => {
+  return (
+    console.log(videos)
+    
+    // <div>
+      
+    //   <Stack
+    //     direction="row" 
+    //     flexWrap="wrap"
+    //     justifyContent="start"
+    //     gap={2}
+    //   >
+    //     {videos.map((item, idx) => (
+    //       <Box key={idx}>
+    //         {item.id.videoId && <VideoCard video={item} />}
+    //         {item.id.channelId && <ChannelCard channelDetail={item} />}
+    //       </Box>
+    //     ))}
+    //   </Stack>
+    // </div>
+  )
+}
+
+export default Videos
